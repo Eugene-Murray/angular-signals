13ab375a8354185747f02fc5eae0f3dc256326a3
Eugene Murray <eugene.murray@gmail.com>
2025-05-14 19:02:37 +0100

    signal state

--- src/app/signal-state-lightweight/signal-state-lightweight.component.ts
+++ src/app/signal-state-lightweight/signal-state-lightweight.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { patchState, signalState } from '@ngrx/signals';
 
 @Component({
   selector: 'app-signal-state-lightweight',
   imports: [],
   templateUrl: './signal-state-lightweight.component.html',
-  styleUrl: './signal-state-lightweight.component.css'
+  styleUrl: './signal-state-lightweight.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignalStateLightweightComponent {
-
+readonly state = signalState({ count: 0 });
+ 
+  increment(): void {
+    patchState(this.state, (state) => ({ count: state.count + 1 }));
+  }
+ 
+  decrement(): void {
+    patchState(this.state, (state) => ({ count: state.count - 1 }));
+  }
+ 
+  reset(): void {
+    patchState(this.state, { count: 0 });
+  }
 }
